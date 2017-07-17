6f4130c931402cecf6a63355d680a0e59e816ce1
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 22:06:02 +0000

    intregrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
      end 
   end
+  
+  def copyright_generator
+    DckrakenViewTool::Renderer.copyright 'Donovan Bracken', 'All rights reserved'
+  end
+  
 end
