52a32e9cc2c57f14b01a0a94b92d657e7495da4b
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 20:16:57 +0000

    finished carrierwave installation

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,8 +6,8 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
-  mounted_uploader :thumb_image, PortfolioUploader
-  mounted_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
 
   def self.angular
