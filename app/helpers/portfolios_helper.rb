92367b342c8baf9c81fb5e2a3af1acd82a9942e9
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 21:11:21 +0000

    updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -2,7 +2,6 @@ module PortfoliosHelper
   def image_generator(height:, width:)
     "http://placehold.it/#{height}x#{width}"
   end
-
   def portfolio_img img, type
     if img.model.main_image? || img.model.thumb_image?
       img
@@ -12,4 +11,5 @@ module PortfoliosHelper
       image_generator(height: '600', width: '400')
     end
   end
+
 end
